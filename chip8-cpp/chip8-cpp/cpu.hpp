c2bf2ac183ecfcee9808d24f0bf5ffaf81bf96ff
Spencer Austman <spencerlgaustman@gmail.com>
2018-05-05 02:14:01 -0700

    Finished adding sample code and fixed typo

--- chip8-cpp/chip8-cpp/cpu.hpp
+++ chip8-cpp/chip8-cpp/cpu.hpp
@@ -6,6 +6,9 @@ class CPU
 	public:
 		void initialize();
 		void cycle();
+		bool draw_flag;
+		
+	private:
 		unsigned char registers[Constants::REGISTER_COUNT]; // 16 8-bit registers (last is flag)
 		unsigned short I; // Index register
 		unsigned short pc; // Program Counter
@@ -16,6 +19,4 @@ class CPU
 		unsigned char gfx[Constants::SCREEN_WIDTH * Constants::SCREEN_HEIGHT]; // Chip 8 screen size is usually 64 * 32
 		unsigned char delay_timer;
 		unsigned char sound_timer;
-		
-	private:
 };
